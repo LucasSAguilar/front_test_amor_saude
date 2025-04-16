11524bb322f8f3a7c0cc7c9e81551c3f9e10c93a
Lucas Aguilar <lucassaguilar01@gmail.com>
2025-04-16 15:24:09 -0300

    applying the action of hiding and showing the password

--- src/app/features/auth/components/formfield/form-field.component.ts
+++ src/app/features/auth/components/formfield/form-field.component.ts
@@ -1,11 +1,11 @@
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formfield',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.css'
 })
@@ -16,4 +16,16 @@ export class FormFieldComponent {
   @Input() control!: FormControl;
   @Input() errorMessage: string = '';
   @Input() id: string = '';
+  @Input() showToggle = false;
+
+  showPassword: boolean = false;
+
+  get currentInputType() {
+    if (this.type !== 'password') return this.type;
+    return this.showPassword ? 'text' : 'password';
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
